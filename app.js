be71a8239616e83f5b4b563a644af9f8d3ab8f2b
Liang jimenez <liangjimenez@yahoo.com>
2023-12-04 11:27:58 -0500

    a6 Mngo fa23

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 import express from "express";
-import HelloRoutes from "./hello.js";
+import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
-import cors from "cors";
 import "dotenv/config";
-// import session from "express-session";
+import session from "express-session";
 
 const app = express();
-
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://a6--glittery-llama-b0aba7.netlify.app",
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
-
-Lab5(app);
+UserRoutes(app);
 ModuleRoutes(app);
+Lab5(app);
+Hello(app);
 CourseRoutes(app);
-HelloRoutes(app);
 app.listen(process.env.PORT || 4000);
